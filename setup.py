c92a9e0358cc0d4158dcd1709e56da3d1ee45c3d
Timothée Jeannin <timojeajea@gmail.com>
2019-01-07 16:14:49 +0100

    Bumped version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='apiflash',
     packages=['apiflash'],
-    version='0.1.2',
+    version='0.1.3',
     url='https://apiflash.com/',
     project_urls=OrderedDict((
         ('Documentation', 'https://apiflash.com/documentation'),
